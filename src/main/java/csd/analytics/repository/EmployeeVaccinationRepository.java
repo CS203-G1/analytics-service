da5e0b41019915929f44b850a71bb2d9e1d277bb
Felice <felicepng2010@gmail.com>
2021-09-18 09:29:38 +0800

    Add EmployeeVaccination repository

---
+++ src/main/java/csd/analytics/repository/EmployeeVaccinationRepository.java
@@ -0,0 +1,14 @@
+package csd.analytics.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import csd.analytics.model.EmployeeVaccination;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EmployeeVaccinationRepository extends JpaRepository<EmployeeVaccination, UUID> {
+    List<EmployeeVaccination> findByEmployeeId(UUID employeeId);
+}

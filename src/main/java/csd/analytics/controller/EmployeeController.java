df5a1f059d2d8d89799b0543a91c03a1f61f5ef6
Jerald <jeraldssw@gmail.com>
2021-09-22 22:15:30 +0800

    Fix error in DELETE mapping due to incorrect path variable and refactor EmployeeController

--- src/main/java/csd/analytics/controller/EmployeeController.java
+++ src/main/java/csd/analytics/controller/EmployeeController.java
@@ -1,18 +1,18 @@
 package csd.analytics.controller;
 
-import csd.analytics.exception.EmployeeNotFoundException;
-import csd.analytics.exception.ResourceNotFoundException;
-import csd.analytics.model.Employee;
-import csd.analytics.service.EmployeeService;
-
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import csd.analytics.model.Employee;
+import csd.analytics.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
@@ -28,26 +28,18 @@ public class EmployeeController {
         return employeeService.addEmployee(employee);
     }
 
-    @GetMapping("/employees/{id}")
-    public ResponseEntity<?> getEmployeeById(@PathVariable UUID id) {
-        try {
-            return new ResponseEntity<>(employeeService.getEmployeeById(id), HttpStatus.OK);
-        } catch (ResourceNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/employees/{id}")
-    public void delete(@PathVariable(value = "employeeId") UUID id) {
-        try {
-            employeeService.deleteEmployeeByid(id);
-        } catch (EmptyResultDataAccessException e) {
-            throw new EmployeeNotFoundException(id);
-        }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable UUID employeeId) {
+        return employeeService.getEmployeeById(employeeId);
     }
 
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void delete(@PathVariable(value = "employeeId") UUID employeeId) {
+        employeeService.deleteEmployeeByid(employeeId);
     }
 }

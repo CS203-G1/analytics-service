dd76a4323415ffed3ec7c3d43c654d2464e677c3
Felice <felicepng2010@gmail.com>
2021-09-22 21:08:59 +0800

    Add GET mapping for EmployeeVaccination

---
+++ src/main/java/csd/analytics/controller/EmployeeVaccinationController.java
@@ -0,0 +1,33 @@
+package csd.analytics.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import csd.analytics.exception.EmployeeVaccinationNotFoundException;
+import csd.analytics.model.EmployeeVaccination;
+import csd.analytics.service.EmployeeVaccinationService;
+
+public class EmployeeVaccinationController {
+    private EmployeeVaccinationService employeeVaccinationService;
+
+    @Autowired
+    public EmployeeVaccinationController(EmployeeVaccinationService employeeVaccinationService) {
+        this.employeeVaccinationService = employeeVaccinationService;
+    }
+
+    @GetMapping("/employees/{employeeId}/employee-vaccination/{employeeVaccinationId}")
+    public EmployeeVaccination getEmployeeVaccination(@PathVariable(value = "employeeId") UUID employeeId,
+                            @PathVariable(value = "employeeVaccinationId") UUID employeeVaccinationId) {
+        return employeeVaccinationService.getEmployeeVaccination(employeeId, employeeVaccinationId);
+    }
+}

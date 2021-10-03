439feb69caff3a9511f3a7b33a75e687977318f3
Jerald <jeraldssw@gmail.com>
2021-10-03 22:10:41 +0800

    Remove UPDATE method for EmployeeService

--- src/main/java/csd/analytics/service/EmployeeService.java
+++ src/main/java/csd/analytics/service/EmployeeService.java
@@ -8,6 +8,5 @@ import csd.analytics.model.Employee;
 public interface EmployeeService {
     List<Employee> getAllEmployeesByDepartmentId(UUID departmentId);
     Employee getEmployeeByDepartmentId(UUID departmentId, UUID employeeId);
-    Employee updateEmployeeByDepartmentId(UUID departmentId, UUID employeeId, Employee employee);
     Employee getEmployeeById(UUID employeeId);
 }

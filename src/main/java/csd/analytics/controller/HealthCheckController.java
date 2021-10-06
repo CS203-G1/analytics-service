4ec80efb21bc67dcfdf3d9e70f15fcfb8c722167
Jerald <jeraldssw@gmail.com>
2021-10-06 17:21:53 +0800

    Add @RestController annotation in health check controller

--- src/main/java/csd/analytics/controller/HealthCheckController.java
+++ src/main/java/csd/analytics/controller/HealthCheckController.java
@@ -3,7 +3,9 @@ package csd.analytics.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthCheckController {
     @GetMapping("/healthcheck")
     public ResponseEntity<String> getHealthCheck() {

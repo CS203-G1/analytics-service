02752f2920050b00d5dc55d867e44258249c6005
Jerald <jeraldssw@gmail.com>
2021-10-07 16:08:08 +0800

    Rename CovidDataRepository and create CompanyRepository

--- src/main/java/csd/analytics/repository/CompanyRepository.java
+++ src/main/java/csd/analytics/repository/CompanyRepository.java
@@ -1,13 +1,14 @@
 package csd.analytics.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import csd.analytics.model.CovidData;
+import csd.analytics.model.Company;
 
 @Repository
-public interface CovidDataRepostiory extends JpaRepository<CovidData, UUID> {
-
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    List<UUID> getAllCompanyIds();
 }

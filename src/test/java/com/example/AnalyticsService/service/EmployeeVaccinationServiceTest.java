c8f5c395ffe43e7f355b1c1ccc347bba7151d910
Felice <felicepng2010@gmail.com>
2021-10-10 23:12:35 +0800

    Remove UPDATE request in Employee service

--- src/test/java/com/example/AnalyticsService/service/EmployeeVaccinationServiceTest.java
+++ src/test/java/com/example/AnalyticsService/service/EmployeeVaccinationServiceTest.java
@@ -1,10 +1,23 @@
 package com.example.AnalyticsService.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import csd.analytics.model.Employee;
+import csd.analytics.model.EmployeeVaccination;
 import csd.analytics.repository.EmployeeVaccinationRepository;
 import csd.analytics.service.EmployeeServiceImpl;
 import csd.analytics.service.EmployeeVaccinationServiceImpl;

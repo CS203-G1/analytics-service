9f22a5a087a859f31c884c502c98ea8e761eae82
Felice <felicepng2010@gmail.com>
2021-10-10 23:13:44 +0800

    Add tests for EmployeeVaccination

--- src/test/java/com/example/AnalyticsService/service/EmployeeVaccinationServiceTest.java
+++ src/test/java/com/example/AnalyticsService/service/EmployeeVaccinationServiceTest.java
@@ -2,12 +2,13 @@ package com.example.AnalyticsService.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import csd.analytics.enumerator.HealthStatus;
+import csd.analytics.enumerator.VaccinationStatus;
 import csd.analytics.model.Employee;
 import csd.analytics.model.EmployeeVaccination;
 import csd.analytics.repository.EmployeeVaccinationRepository;
@@ -32,4 +35,27 @@ public class EmployeeVaccinationServiceTest {
 
     @InjectMocks
     EmployeeVaccinationServiceImpl employeeVaccinationService;
+
+    @Test
+    public void getEmployeeVaccinations_EmployeeVaccinationsExist_ReturnListOfEmployeeVaccinations() {
+        UUID employeeId = UUID.randomUUID();
+        List<EmployeeVaccination> allEmployeeVaccinationsInEmployee = new ArrayList<EmployeeVaccination>();
+        VaccinationStatus vaccinationStatus = VaccinationStatus.NOT_VACCINATED;
+        HealthStatus healthStatus = HealthStatus.COVID;
+        Employee employee = new Employee(employeeId, allEmployeeVaccinationsInEmployee, null, "John Doe", vaccinationStatus, null, healthStatus, LocalDateTime.now());
+
+        UUID employeeVaccinationId = UUID.randomUUID();
+        EmployeeVaccination employeeVaccination = new EmployeeVaccination(employeeVaccinationId, employee, null, 0, LocalDateTime.now());
+
+        allEmployeeVaccinationsInEmployee.add(employeeVaccination);
+
+        when(employeeVaccinations.findByEmployeeId(employeeId)).thenReturn(employee.getEmployeeVaccinations());
+
+        List<EmployeeVaccination> employeeVaccinationsForEmployee = employeeVaccinationService.getEmployeeVaccinations(employeeId);
+
+        assertNotNull(employeeVaccinationsForEmployee);
+        assertEquals(1, employeeVaccinationsForEmployee.size());
+
+        verify(employeeVaccinations, times(1)).findByEmployeeId(employeeId);
+    }
 }

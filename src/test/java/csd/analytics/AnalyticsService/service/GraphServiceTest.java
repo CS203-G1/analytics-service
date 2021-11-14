5ef25f04163feb505cfe3835b42b3f61f4838d83
Jerald <jeraldssw@gmail.com>
2021-11-14 23:46:20 +0800

    Create testing for CovidDataService and create custom mockable ClockUtil class

--- src/test/java/csd/analytics/AnalyticsService/service/GraphServiceTest.java
+++ src/test/java/csd/analytics/AnalyticsService/service/GraphServiceTest.java
@@ -1,5 +1,23 @@
 package csd.analytics.AnalyticsService.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import csd.analytics.repository.EmployeeVaccinationRepository;
+import csd.analytics.service.EmployeeServiceImpl;
+import csd.analytics.service.EmployeeVaccinationServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 public class GraphServiceTest {
-    
+    @Mock
+    EmployeeVaccinationRepository employeeVaccinations;
+
+    @Mock
+    EmployeeServiceImpl employeeService;
+
+    @InjectMocks
+    EmployeeVaccinationServiceImpl employeeVaccinationService;
+
 }
